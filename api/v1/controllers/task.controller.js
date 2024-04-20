5c169b09815341d504ea7486fd351bf07b03a9d8
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-04-21 01:34:33 +0700

    Thêm mới công việc

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -123,3 +123,22 @@ module.exports.changeMulti = async (req, res) => {
         });
     }
 };
+
+// [POST] /api/v1/tasks/create
+module.exports.create = async (req, res) => {
+    try {
+        const task = new Task(req.body);
+        const data = await task.save();
+
+        res.json({
+            code: 200,
+            message: "Cap nhat trang thai thanh cong",
+            data: data
+        });
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Error"
+        });
+    }
+};
