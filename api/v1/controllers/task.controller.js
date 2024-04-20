6ebc975f1182f50349ee3ce054bc24693d63be7e
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-04-21 01:52:29 +0700

    Chỉnh sửa công việc

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -142,3 +142,24 @@ module.exports.create = async (req, res) => {
         });
     }
 };
+
+// [PATCH] /api/v1/tasks/edit/:ids
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        await Task.updateOne({
+            _id: id
+        }, req.body);
+
+        res.json({
+            code: 200,
+            message: "Cap nhat thanh cong"
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Error"
+        })
+    }
+};
