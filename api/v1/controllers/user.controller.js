d8ab44a4a0cff907035cd03458c6a008f1ccba05
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-04-26 01:47:30 +0700

    Tính năng lấy lại mk OTP qua email.

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,5 +1,9 @@
 const md5 = require("md5");
 const User = require("../models/user.model");
+const ForgotPassword = require("../models/forgot-password.model");
+
+const generateHelper = require("../../../helpers/generate");
+const sendMailHelper = require("../../../helpers/sendMail");
 
 // [POST] /api/v1/users/register
 module.exports.register = async (req, res) => {
@@ -70,3 +74,80 @@ module.exports.login = async (req, res) => {
     });
 };
 
+// [POST] /api/v1/users/forgotPassword
+module.exports.forgotPassword = async (req, res) => {
+    const email = req.body.email;
+
+    const user = User.findOne({
+        email: email,
+        deleted: false
+    });
+
+    if(!user) {
+        res.json({
+            code: 400,
+            message: "Email không tồn tại!"
+        });
+    }
+
+    const otp = generateHelper.generateRandomNumber(8);
+
+    const timeExpire = 5;
+
+    // Luu data vao database
+    const objectforgotPassword = {
+        email: email,
+        otp: otp,
+        expireAt: Date.now() + timeExpire*60
+    };
+
+    // console.log(objectforgotPassword);
+    const forgotPassword = new ForgotPassword(objectforgotPassword);
+    await forgotPassword.save();
+
+    // Gui OTP qua email user
+    const subject = "Mã OTP xác minh lấy lại mật khẩu";
+    const html = `
+        Mã OTP để lấy lại mật khẩu của bạn là <b>${otp}</b> (Sử dụng trong ${timeExpire} phút).
+        Vui lòng không chia sẻ mã OTP với bất kỳ ai.
+    `;
+
+    sendMailHelper.sendMail(email, subject, html);
+
+    res.json({
+        code: 200,
+        message: "Đã gửi mã OTP qua email!"
+    });
+};
+
+// [POST] /api/v1/users/otpPassword
+module.exports.otpPassword = async (req, res) => {
+    const email = req.body.email;
+    const otp = req.body.otp;
+
+    const result = await ForgotPassword.findOne({
+        email: email,
+        otp: otp
+    });
+
+    if(!result) {
+        res.json({
+            code: 400,
+            message: "OTP không hợp lệ!"
+        });
+        return;
+    }
+
+    const user = await User.findOne({
+        email: email
+    });
+
+    const token = user.token;
+    res.cookie("token", token);
+
+    res.json({
+        code: 200,
+        message: "Xác thực thành công.",
+        token: token
+    });
+};
